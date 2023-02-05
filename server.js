00741c4469512554277e3015e07798acbaa8155c
Jamie Tiblis <jajamie@live.unc.edu>
2023-02-05 17:05:05 -0500

    Implemented fs module, set up server with status code, header, info extracted from file

--- server.js
+++ server.js
@@ -15,34 +15,29 @@ var arg = mini(process.argv.slice(2));
 const port = (arg.port || 3000);
 
 // Use the fs module to create an arrow function using `fs.readFile`.
-
-
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
-
-
-
-
+fs.readFile('./public/index.html','utf8', (error, info) => {
+  while (error) {
+    return console.error(error);
+  }
+  console.log(info);
 
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
+const server = http.createServer((req, res) => {
 // The function should have three responses: 
 // 1. status code 200, 
-// 2. set a header with content type `text/html`, and 
-// 3. end with the data that you are reading in from ./public/index.html.
-
-
-
-
+res.statusCode=200;
+// 2. set a header with content type `text/html`, and   
+res.setHeader('Content-Type', 'text/html');
+// 3. end with the data that you are reading in from ./public/index.html.  
+res.end(info);
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-
-
-
 
 // That's it! You're all done!
