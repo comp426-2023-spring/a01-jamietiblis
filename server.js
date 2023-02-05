bcb470fcbfdca8cd420ba0da66c5864741568f08
Jamie Tiblis <jajamie@live.unc.edu>
2023-02-05 17:08:44 -0500

    Starting the server on defined port

--- server.js
+++ server.js
@@ -39,5 +39,6 @@ res.end(info);
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-
+server.listen(port);
+console.log(`Server listening on port ${port}`); 
 // That's it! You're all done!
